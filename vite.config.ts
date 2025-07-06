58be63b0a7a1b6d4eaf184615dd7382829d2d4ca
hayeong <hayeong.jeon95@gmail.com>
2025-07-06 18:21:15 +0900

    저장소명 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mobile-wedding-invitation/',
   plugins: [react(), tsconfigPaths(), svgr()],
 });
