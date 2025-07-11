253191ab39572fb28632cc50c7b90c63a3f96d58
hayeong <hayeong.jeon95@gmail.com>
2025-07-11 13:11:53 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState, Suspense, lazy } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { NavermapsProvider } from 'react-naver-maps';
 import { Heading1 } from '@/components/Text';
 import Wrapper from '@/components/Wrapper';
